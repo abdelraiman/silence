8cb6c19e613f547af9930bf263a759560edb1307
abdelraiman <abdelrahman.abouzied@student.saedubai.com>
2025-03-10 01:21:38 +0400

    asset

--- Assets/scripts/Assetsbundle.cs
+++ Assets/scripts/Assetsbundle.cs
@@ -14,22 +14,21 @@ public class Assetsbundle : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        LoadAssetsBunndle();
+    }
+
+    void LoadAssetsBunndle()
+    {
         combinedPath = Path.Combine(Application.streamingAssetsPath, folderPath, fileName);
 
         if (File.Exists(combinedPath))
         {
             stuf = AssetBundle.LoadFromFile(combinedPath);
-            Debug.Log("ahuh stuff is here"); 
+            Debug.Log("ahuh stuff is here");
         }
         else
         {
             Debug.Log("i dont see any of those files in the assetsbunndle" + combinedPath);
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
