0dadee2dde1dd3d695e19e9bc4912887b20592c9
abdelraiman <abdelrahman.abouzied@student.saedubai.com>
2025-03-27 14:48:17 +0400

    finelized takedown system

--- Assets/scripts/takedown script.cs
+++ Assets/scripts/takedown script.cs
@@ -9,21 +9,21 @@ public class takedownscript : MonoBehaviour
     public LayerMask Enemy;
     public float num = 1;
     public KeyCode interact = KeyCode.E;
-    public Animator anim;
+    RaycastHit hit;
+    public GameObject enemy;
     
     void Start()
     {
-        //anim = GetComponent<Animator>();
+       
     }
 
     void Update()
     {
-        
-        RaycastHit hit;
         if (Physics.Raycast(transform.position, transform.forward, out hit, num))
         {
             if (hit.collider.tag != "enemy")
             {
+                enemy = hit.collider.gameObject;
                 takedown = false;
             }
             else
@@ -44,7 +44,11 @@ public class takedownscript : MonoBehaviour
         if (Input.GetKeyDown(interact) && takedown)
         {
             Debug.Log("assasinated");
-            anim.SetTrigger("TakedownTrigger");
+            if (enemy != null)
+            {
+                enemy.GetComponentInParent<enemyTakedown>().tookdown();
+                Debug.Log("enemy is there");
+            }
         }
     }
 
