0e404746f5258788b8a2ae84c3d4351cc89a7e26
abdelraiman <abdelrahman.abouzied@student.saedubai.com>
2025-04-18 18:44:42 +0400

    creat enemy line of sight

--- Assets/scripts/Enemy/Enemy.cs
+++ Assets/scripts/Enemy/Enemy.cs
@@ -11,17 +11,39 @@ public class Enemy : MonoBehaviour
    
     [SerializeField] private string curentstate;
     public path paath;
+    [SerializeField] private GameObject player;
+    public float sightdistance = 20f;
+    public float fov = 85;
 
     void Start()
     {
         stateMachine = GetComponent<StateMachine>();
         agent = GetComponent<NavMeshAgent>();
         stateMachine.Initialise();
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        canseeplayer();
+    }
+
+    public bool canseeplayer()
+    {
+        if (player != null)
+        {
+            if (Vector3.Distance(transform.position,player.transform.position) < sightdistance)
+            {
+                Vector3 targetDirection = player.transform.position - transform.position;
+                float angelToplayer = Vector3.Angle(targetDirection, transform.forward);
+                if (angelToplayer >= -fov && angelToplayer <= fov)
+                {
+                    Ray ray = new Ray(transform.position, targetDirection);
+                    Debug.DrawRay(ray.origin,ray.direction * sightdistance);
+                }
+            }
+        }
+        return true;
     }
 }
