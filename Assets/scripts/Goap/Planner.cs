bf61d4692374b840bb12624f508ea7b7d9431ad8
abdelraiman <abdelrahman.abouzied@student.saedubai.com>
2025-07-30 01:39:37 +0400

    set up planing choosing and geting rid of actions and effects

--- Assets/scripts/Goap/Planner.cs
+++ Assets/scripts/Goap/Planner.cs
@@ -1,6 +1,3 @@
-using Newtonsoft.Json;
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -13,26 +10,69 @@ public class GoapPlanner : GoapPlannerI
 {
     public ActionPlan Plan(GoapAgent agent, HashSet<Goals> goals, Goals mostRecentGoal = null)
     {
-     List<Goals> OrderedGoals = goals
-    .Where(g => g.DesiredEffect.Any(b => !b.Evaluate()))
-    .OrderByDescending(g => g == mostRecentGoal ? g.Priority - 0.01f : g.Priority)
-    .ToList();
+        List<Goals> OrderedGoals = goals
+       .Where(g => g.DesiredEffect.Any(b => !b.Evaluate()))
+       .OrderByDescending(g => g == mostRecentGoal ? g.Priority - 0.01f : g.Priority)
+       .ToList();
 
         foreach (var goal in OrderedGoals)
         {
-            Node NoalNode = new Node(null, null, goal.DesiredEffect, 0);
+            Node GoalNode = new Node(null, null, goal.DesiredEffect, 0);
 
             if (FindPath(GoalNode, agent.actions))
             {
+                if (GoalNode.IsLeafDead) continue;
 
+                Stack<AgentAction> actoinStack = new Stack<AgentAction>();
+                while (GoalNode.Leaves.Count > 0)
+                {
+                    var cheapestLeaf = GoalNode.Leaves.OrderBy(leaf => leaf.Cost).First();
+                    GoalNode = cheapestLeaf;
+                    actoinStack.Push(cheapestLeaf.Action);
+                }
+
+                return new ActionPlan(goal, actoinStack,GoalNode.Cost);
             }
         }
-    } 
+        Debug.LogWarning("no plan found");
+        return null;
+    }
 
     bool FindPath(Node parent, HashSet<AgentAction> actions)
     {
-        foreach (var action in actions)
-        var requiredEffect = parent.RequiredEffect;
+        var orderedActions = actions.OrderBy(a => a.Cost);
+
+        foreach (var action in orderedActions)
+        {
+            var requiredEffects = parent.RequiredEffects;
+
+            requiredEffects.RemoveWhere(b => b.Evaluate());
+
+            if (requiredEffects.Count == 0)
+            {
+                return true;
+            }
+
+            if (action.Effects.Any(requiredEffects.Contains))
+            {
+                var newRequiredEffects = new HashSet<AIBeliefs>(requiredEffects);
+                newRequiredEffects.ExceptWith(action.Effects);
+                newRequiredEffects.UnionWith(action.Preconditions);
+
+                var newAvailableAction = new HashSet<AgentAction>(actions);
+                newAvailableAction.Remove(action);
+
+                var newNode = new Node(parent, action, newRequiredEffects, parent.Cost + action.Cost);
+
+                if (FindPath(newNode, newAvailableAction))
+                {
+                    parent.Leaves.Add(newNode);
+                    newRequiredEffects.ExceptWith(newNode.Action.Preconditions);
+                }
+                if (newRequiredEffects.Count == 0) return true;
+            }
+        }
+        return false;
     }
 }
 
@@ -40,7 +80,7 @@ public class Node
 {
     public Node Parent { get; }
     public AgentAction Action { get; }
-    public HashSet<AIBeliefs> RequiredEffect { get; }
+    public HashSet<AIBeliefs> RequiredEffects { get; }
     public List<Node> Leaves { get; }
     public float Cost { get; }
 
@@ -50,14 +90,14 @@ public class Node
     {
         Parent = parent;
         Action = action;
-        RequiredEffect = new HashSet<AIBeliefs>(effects);
+        RequiredEffects = new HashSet<AIBeliefs>(effects);
         Leaves = new List<Node>(); ;
         Cost = cost;
     }
 }
 public class ActionPlan
 {
-    public Goals AgentGoal {  get; }
+    public Goals AgentGoal { get; }
     public Stack<AgentAction> Actions { get; }
     public float TotalCost { get; set; }
 
